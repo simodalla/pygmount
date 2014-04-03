d5fe008fb8c7bf754770175f889e6faa15473d22
Simone Dalla <simodalla@gmail.com>
2014-04-03 10:10:32 +0200

    migration from mercurial

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@
 
 import os
 import sys
+import shutil
+import traceback
+
+import pygmount
 
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+
+version = pygmount.__version__
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
@@ -20,7 +26,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 setup(
     name='pygmount',
     version='0.1.0',
-    description='Python Boilerplate contains all the boilerplate you need to create a Python package.',
+    description='Software for graphical mount network shares.',
     long_description=readme + '\n\n' + history,
     author='Simone Dalla',
     author_email='simodalla@gmail.com',
@@ -32,19 +38,50 @@ setup(
     include_package_data=True,
     install_requires=[
     ],
+    entry_points={
+        'console_scripts': [
+            'mount-smb-shares = pygmount.bin.mount_smb_shares:main'
+        ]
+    },
     license="BSD",
     zip_safe=False,
-    keywords='pygmount',
+    keywords='pygmount samba zenity pyzenity gnome mint',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Environment :: X11 Applications :: GTK',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Other Audience',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
+        'Natural Language :: Italian',
+        'Operating System :: POSIX :: Linux',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Topic :: Desktop Environment',
+        'Topic :: Desktop Environment :: Gnome',
+        'Topic :: Office/Business',
+        'Topic :: System',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Shells',
+        'Topic :: Utilities',
     ],
     test_suite='tests',
 )
+
+print("########")
+if 'install' in sys.argv:
+    print("*********")
+    ABSOLUTE_PATH = '%s' % os.path.abspath(
+        os.path.dirname(__file__)).replace('\\', '/')
+    desktop_applications = '/usr/share/applications/'
+    if os.path.exists(desktop_applications):
+        try:
+            shutil.copy(ABSOLUTE_PATH + '/media/mount-smb-shares.desktop',
+                        desktop_applications)
+        except IOError:
+            traceback.print_exc(file=sys.stdout)
